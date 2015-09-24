782aed3af5f3d161d2a613c010d9b9eb60da62b1
allenice <994298628@qq.com>
2015-09-25 00:43:51 +0800

    test

---
+++ nodejs/gulp2/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    through = require('through2');
+
+function prefixText(text) {
+    return through.obj(function(file, en, cb) {
+        console.log(file.isStream());
+        console.log(file.isBuffer());
+        cb();
+    });
+}
+
+gulp.task('task1', function() {
+    console.log('task1 run');
+   return gulp.src('./package.json').pipe(prefixText('Allenice')); 
+});
+
+gulp.task('task2', ['task1'], function() {
+    console.log('task2 run');
+});
+
+gulp.task('default', function() {
+    console.log('hello world!');
+});
