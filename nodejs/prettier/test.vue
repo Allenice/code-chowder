2d33677d10ae459d215491372a7feeba3bfdbac2
Allenice Mo <994298628@qq.com>
2020-08-13 14:33:00 +0800

    格式化

---
+++ nodejs/prettier/test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        aaaa
+        <p test="1" title="2" title2="333"></p>
+    </div>
+</template>
+<style lang="scss">
+.test {
+    width: 0.8;
+    height: 0;
+    color: #FFF;
+}
+</style>
