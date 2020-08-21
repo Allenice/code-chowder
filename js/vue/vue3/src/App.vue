11f100e541a18f29ab85e083c56e964e43306a77
Allenice Mo <994298628@qq.com>
2020-08-21 12:27:52 +0800

    vue 3 test

--- js/vue/vue3/src/App.vue
+++ js/vue/vue3/src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <icon :data="vueIcon" original></icon>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "./components/HelloWorld.vue";
+import vueIcon from "@icon/vue.svg";
+console.log(vueIcon);
 
 @Options({
   components: {
     HelloWorld
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  vueIcon = vueIcon;
+}
 </script>
 
 <style lang="scss">
--- js/vue/vue3/src/main.ts
+++ js/vue/vue3/src/main.ts
@@ -2,12 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button } from 'element-ui';
+import { Button } from "element-ui";
+import { VueSvgIcon } from "@yzfe/vue-svgicon";
+import "@yzfe/vue-svgicon/dist/index.css";
 
 const app = createApp(App);
 
 app.component(Button.name, Button);
+app.component('icon', VueSvgIcon)
 
-app.use(store)
+app
+  .use(store)
   .use(router)
   .mount("#app");
